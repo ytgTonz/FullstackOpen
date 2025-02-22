9c3ad63d961b16c1e21e0e5582b5441ca61d5af8
Vesa Jolkkonen <vesa.jolkkonen@hotmail.com>
2025-02-22 12:15:38 +0200

    add part3-2

--- index.js
+++ index.js
@@ -19,7 +19,16 @@ let notes = [
   },
 ]
 
+const requestLogger = (request, response, next) => {
+  console.log('Method:', request.method)
+  console.log('Path:  ', request.path)
+  console.log('Body:  ', request.body)
+  console.log('---')
+  next()
+}
+
 app.use(express.json())
+app.use(requestLogger)
 
 app.get('/', (request, response) => {
   response.send('<h1>Hello World!</h1>')
@@ -72,6 +81,12 @@ app.delete('/api/notes/:id', (request, response) => {
 
   response.status(204).end()
 })
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+
+app.use(unknownEndpoint)
 
 const PORT = 3001
 app.listen(PORT, () => {
