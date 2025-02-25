e2570483ec5b9388374e3a5848fb3cbdf60273d2
Vesa Jolkkonen <vesa.jolkkonen@hotmail.com>
2025-02-25 20:38:44 +0200

    Add part3-3

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const cors = require('cors')
 const express = require('express')
 const app = express()
 
@@ -27,8 +28,10 @@ const requestLogger = (request, response, next) => {
   next()
 }
 
-app.use(express.json())
 app.use(requestLogger)
+app.use(express.static('dist'))
+app.use(cors())
+app.use(express.json())
 
 app.get('/', (request, response) => {
   response.send('<h1>Hello World!</h1>')
@@ -88,7 +91,7 @@ const unknownEndpoint = (request, response) => {
 
 app.use(unknownEndpoint)
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
